db837b2ea9577b9f51e0a0bc9b5341505926eab2
Abbey Heflin <aeh6m@hscmail.mcc.virginia.edu>
2016-11-18 12:26:51 -0500

    asgnmt3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## It is good to cache results so that your function doesnt have to rerun large data sets.
+##My functions will allow me to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## This is my function to store my data
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv = NULL
+        set = function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get = function() x
+        setInverse = function(inverse) inv <<- inverse
+        getInverse = function() inv
+        list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv = x$getInverse()
+        if (!is.null(inv)){
+                message("Retrieving Data")
+                return(inv)
+        }
+        mat <- x$get()
+        inv <- solve(mat, ...)
+        x$setInverse(inv)
+        inv
 }
